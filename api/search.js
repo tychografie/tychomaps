0e95a88241ae61842fce7dec9f25237a14822737
tychografie <hoi@tycholitjens.nl>
2024-05-08 17:20:49 +0200

    Rewrite to pipeline madness + staticmode support

--- api/search.js
+++ api/search.js
@@ -2,100 +2,202 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-function logSearch(query, mapsQuery, numPlaces) {
-    const filePath = path.join(__dirname, 'searchLogs.json');
-    const data = {
-        query,
-        mapsQuery,
-        numPlaces,
-        timestamp: new Date()
-    };
 
-    fs.readFile(filePath, (err, content) => {
-        let searches = [];
-        if (!err && content.length > 0) {
-            searches = JSON.parse(content.toString());
-        }
-        searches.push(data);
-        fs.writeFile(filePath, JSON.stringify(searches, null, 2), err => {
-            if (err) {
-                console.error('Error writing to file', err);
-            }
-        });
-    });
+const preprocess = async (userQuery) => {
+    // clean incoming data from user first
+
+
+    // when finalized user input, do AI request
+    const mapsQuery = await aiRequest(userQuery)
+
+    // check AI mapsQuery, possibly incorrect, then do another preprocess() call with modified user query
+
+    // const newInput = await aiRequest("aangepaste user input")
+
+    // final query:
+    return mapsQuery
 }
+
+const aiRequest = async (query) => {
+    // Read the query prefix from a separate file
+    const queryPrefix = fs.readFileSync(path.join(__dirname, 'chatgptquery.txt'), 'utf8').trim();
+    const aiResponse = await axios.post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+            model: "gpt-3.5-turbo",
+            messages: [{
+                role: "user",
+                content: `${queryPrefix} ${query}`
+            }],
+            temperature: 0.7
+        },
+        {
+            headers: {
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+
+
+
+    if (aiResponse.data.choices && aiResponse.data.choices.length > 0) {
+        const mapsQuery = aiResponse.data.choices[0].message.content.trim();
+
+        if (mapsQuery.toLowerCase().includes('maps')) {
+
+            throw new Error("🦈 Ha! You might be entering nonsense.");
+
+            // res.status(404).json({ error: '🦈 Ha! You might be entering nonsense.' });
+            // return;
+        }
+        return mapsQuery
+
+
+    } else {
+        throw new Error('No valid response from AI')
+        // res.status(500).json({ error: 'No valid response from AI' });
+    }
+}
+
+
+/**
+ * 
+ * @param {*} aiResponse 
+ * @returns data with {places}
+ */
+
+const mapsRequest = async (mapsQuery) => {
+    let minRating = 4.5
+    if (mapsQuery.toLowerCase().includes("club")) {
+        minRating = 4.0
+        //     mapsQuery = mapsQuery.replace("dit","data")
+    }
+
+    const mapsResponse = await axios.post(
+        'https://places.googleapis.com/v1/places:searchText',
+        {
+            textQuery: mapsQuery,
+            minRating: minRating
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Goog-Api-Key': process.env.GOOGLE_MAPS_API_KEY,
+                'X-Goog-FieldMask': 'places.displayName,places.rating,places.userRatingCount,places.googleMapsUri'
+            }
+        }
+    );
+
+    return mapsResponse.data
+}
+
+/**
+ * 
+ * @param {*} mapsResponse 
+ * @param {*} mapsQuery 
+ * @returns sortedPlaces[]
+ */
+const processor = async (mapsResponse, mapsQuery) => {
+
+    const numPlaces = mapsResponse && mapsResponse.places ? mapsResponse.places.length : 0;
+
+    if (mapsResponse && mapsResponse.places && mapsResponse.places.length === 1) {
+        const placeName = mapsResponse.places[0].displayName.text.toLowerCase();
+
+        const aiResponseWords = mapsQuery.split('+');
+
+        const hasHit = aiResponseWords.some(word => placeName.includes(word));
+        if (hasHit) {
+            throw new Error("Error alert: AI response word found in place name")
+            // res.status(400).json({ error: 'Error alert: AI response word found in place name' });
+            // return;
+        }
+    }
+    console.log(JSON.stringify(mapsResponse))
+    if (mapsResponse && mapsResponse.places) {
+        const filteredPlaces = mapsResponse.places.filter(place => place.userRatingCount > 15 && place.userRatingCount < 1500);
+        const sortedPlaces = filteredPlaces.sort((a, b) => b.rating - a.rating).slice(0, 30);
+
+        return sortedPlaces
+
+    } else {
+        // const aiquery = await aiRequest("een+aangepaste+query+met+andere+woorden")
+        // const mapsPlaces = await mapsRequest("een+aangepaste+query+met+andere+woorden")
+
+        // return await processor(mapsPlaces, mapsQuery)
+
+        throw new Error('No places found')
+        // res.status(404).json({ error: 'No places found', aiResponse: aiResponse.data.choices[0].message.content });
+    }
+}
+
 
 module.exports = async (req, res) => {
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Method not allowed" });
     }
 
-    const { query } = req.body;
+    const { query, staticMode } = req.body;
     if (!query || query.length > 128) {
         return res.status(400).json({ error: "Invalid query length" });
     }
 
     try {
-        // Read the query prefix from a separate file
-        const queryPrefix = fs.readFileSync(path.join(__dirname, 'chatgptquery.txt'), 'utf8').trim();
-
-        const aiResponse = await axios.post(
-            'https://api.openai.com/v1/chat/completions',
-            {
-                model: "gpt-3.5-turbo",
-                messages: [{
-                    role: "user",
-                    content: `${queryPrefix} ${query}`
-                }],
-                temperature: 0.7
-            },
-            {
-                headers: {
-                    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
 
 
-        if (aiResponse.data.choices && aiResponse.data.choices.length > 0) {
-            const mapsQuery = aiResponse.data.choices[0].message.content.trim();
+        // if (staticMode) {
+        //     // return
+        //     const staticData = JSON.parse(fs.readFileSync(path.join(__dirname, 'staticData.json'), 'utf8'));
 
-            if (mapsQuery.toLowerCase().includes('maps')) {
-                res.status(404).json({ error: '🦈 Ha! You might be entering nonsense.' });
-                return;
-            }
+        //     return await processor(staticData)
+
+        //     // return res.status(200).json(staticData);
+        // }
+
+        let mapsQuery;
+        try {
+
+            // mapsQuery = await aiRequest(query) // throws error if maps
+            if (staticMode) {
+            //     mapsQuery = "utrecht+koffie+gezellig"
+
+            //     const placesTester = await mapsRequest(mapsQuery)
+                // const placesTester = {
+                //     "places": [
+                //         {
+                //             "rating": 4.9,
+                //             "googleMapsUri": "https://maps.google.com/?cid=14960975664163707840",
+                //             "userRatingCount": 1323,
+                //             "displayName": {
+                //                 "text": "Pampalini Lunchroom & Coffee - Utrecht",
+                //                 "languageCode": "en"
+                //             }
+                //         }
+                //     ],
+                //     "aiResponse": ""
+                // }
+                const sortedPlaces = await processor(placesTester, mapsQuery);
+
+                return res.status(200).json({ places: sortedPlaces, aiResponse: mapsQuery })
 
 
-            const mapsResponse = await axios.post(
-                'https://places.googleapis.com/v1/places:searchText',
-                {
-                    textQuery: mapsQuery,
-                    minRating: 4.5
-                },
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-Goog-Api-Key': process.env.GOOGLE_MAPS_API_KEY,
-                        'X-Goog-FieldMask': 'places.displayName,places.rating,places.userRatingCount,places.googleMapsUri'
-                    }
-                }
-            );
-
-            const numPlaces = mapsResponse.data && mapsResponse.data.places ? mapsResponse.data.places.length : 0;
-
-            logSearch(query, mapsQuery, numPlaces);
-
-            if (mapsResponse.data && mapsResponse.data.places) {
-                const filteredPlaces = mapsResponse.data.places.filter(place => place.userRatingCount > 15 && place.userRatingCount < 1500);
-                const sortedPlaces = filteredPlaces.sort((a, b) => b.rating - a.rating).slice(0, 30);
-                res.status(200).json({ places: sortedPlaces });
             } else {
-                res.status(404).json({ error: 'No places found' });
             }
-        } else {
-            res.status(500).json({ error: 'No valid response from AI' });
+            mapsQuery = await preprocess(query)
+
+            const places = await mapsRequest(mapsQuery)
+
+            const sortedPlaces = await processor(places, mapsQuery);
+
+            return res.status(200).json({ places: sortedPlaces, aiResponse: mapsQuery })
+
+        } catch (error) {
+            res.status(400).json({ error: error.message, aiResponse: mapsQuery })
+            return;
         }
+
+
     } catch (error) {
         console.error('API request failed:', error);
         res.status(500).json({ error: 'Internal Server Error', message: error.message });
--- js/sendQuery.js
+++ js/sendQuery.js
@@ -1,6 +1,5 @@
-
 function sendQuery() {
-
+    const mapsQuery = document.getElementById('mapsQuery');
     var button = document.getElementById('search-button');
     button.disabled = true;
     button.innerHTML = `
@@ -18,15 +17,15 @@ function sendQuery() {
         return;
     };
 
-    const useStatic = new URLSearchParams(window.location.search).get('useStatic') === 'true';
-    const url = useStatic ? 'staticData.json' : '/api/search';
+    // const useStatic = new URLSearchParams(window.location.search).get('useStatic') === 'true';
+    // const url = useStatic ? 'staticData.json' : '/api/search';
 
-    fetch(url, {
+    fetch('/api/search', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ query: query })
+        body: JSON.stringify({ query: query, staticMode: new URLSearchParams(window.location.search).get('useStatic') })
     })
     .then(response => response.json())
     .then(data => {
@@ -36,16 +35,14 @@ function sendQuery() {
         const totalElement = document.getElementById('total');
         totalElement.textContent = totalPlaces - 5;
 
-        if (data.places && data.places.length) {  // Adjusted for the actual data structure
-            if (data.places && data.places.length) {  // Adjusted for the actual data structure
-                data.places.slice(0, 5).forEach((result, index) => {  // Limit iteration to the first five results
-                    setTimeout(() => {
-                        const resultItem = document.createElement('a');
-                        resultItem.className = 'result-item';
-                        resultItem.href = result.googleMapsUri;
-                        resultItem.target = "_blank";
-
-                        resultItem.innerHTML = `
+        if (data.places && data.places.length) {  
+            data.places.slice(0, 5).forEach((result, index) => { 
+                setTimeout(() => {
+                    const resultItem = document.createElement('a');
+                    resultItem.className = 'result-item';
+                    resultItem.href = result.googleMapsUri;
+                    resultItem.target = "_blank";
+                    resultItem.innerHTML = `
                         <div class="top-row hover:bg-neutral-950 flex justify-between p-5 rounded-md bg-neutral-800 text-white text-lg">
                             <div class="flex items-center space-x-3">
                                 <span class="place-name">${result.displayName.text}</span>
@@ -55,99 +52,25 @@ function sendQuery() {
                             <span class="rating">${result.rating}</span>
                         </div>
                         `;
-                        resultsContainer.appendChild(resultItem);
-                    }, 100 * index);
-                });
-                button.innerHTML = 'Search';
-                button.disabled = false;
-            } else {
-                resultsContainer.innerHTML = '<p>No results found or invalid query. <u><a href="/support">Help me out! 😰</a></u></p>';
-                button.innerHTML = 'Search';
-                button.disabled = false;
-            }
+                    resultsContainer.appendChild(resultItem);
+                }, 100 * index);
+            });
+            button.innerHTML = 'Search';
+            button.disabled = false;
+            
+            mapsQuery.textContent = data.aiResponse;
         } else {
             resultsContainer.innerHTML = '<p>No results found or invalid query. <u><a href="/support">Help me out! 😰</a></u></p>';
             button.innerHTML = 'Search';
             button.disabled = false;
+            mapsQuery.textContent = data.aiResponse;
         }
     })
     .catch(error => {
         console.error('Error:', error);
         document.getElementById('results').innerHTML = '<p>An error occurred fetching the results.</p>';
         button.innerHTML = 'Search';
+        mapsQuery.textContent = data.aiResponse;
         button.disabled = false;
-    });
-}
-
-function evenMoreResults() {
-    var buttonMore = document.getElementById('loadResults');
-    buttonMore.innerHTML = `
-        <div style="display: flex; justify-content: center; align-items: center;">
-            <svg class="animate-spin h-5 w-5 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-        </div>
-    `;
-
-    var modal = document.getElementById('default-modal');
-    modal.classList.add('hidden');
-
-    var query = document.getElementById('query').value.trim();
-    if (query.length < 1) return;
-
-    const useStatic = new URLSearchParams(window.location.search).get('useStatic') === 'true';
-    const url = useStatic ? 'staticData.json' : '/api/search';
-
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ query: query })
-    })
-    .then(response => response.json())
-    .then(data => {
-        const resultsContainer = document.getElementById('results');
-
-        if (data.places && data.places.length) {  // Adjusted for the actual data structure
-            data.places.slice(5, 30).forEach((result, index) => {  // Limit iteration to the first five results
-                setTimeout(() => {
-                    const resultItem = document.createElement('a');
-                    resultItem.className = 'result-item';
-                    resultItem.href = result.googleMapsUri;
-                    resultItem.target = "_blank";
-
-                    resultItem.innerHTML = `
-                    <div class="top-row opacity-90 hover:bg-neutral-950 flex justify-between p-5 rounded-md bg-neutral-800 text-white text-lg">
-                        <div class="flex items-center space-x-3">
-                            <span class="place-name blur-md">${result.displayName.text}</span>
-                            ${result.opening_hours && result.opening_hours.open_now ?
-                            '<span class="relative grid select-none items-center whitespace-nowrap rounded-lg bg-neutral-900 py-1.5 px-3 font-sans text-xs font-bold uppercase text-white">Open Now</span>' : ''}
-                        </div>
-                        <span class="rating">${result.rating}</span>
-                    </div>
-                    `;
-                    resultsContainer.appendChild(resultItem);
-                    window.scrollTo({
-                        top: document.documentElement.scrollHeight,
-                        behavior: 'smooth'
-                    });
-                    if (window.location.href.includes('tycho')) {
-                        resultItem.querySelector('.place-name').classList.remove('blur-md');
-                        resultItem.querySelector('.top-row').style.opacity = 100;
-                    }
-                }, 100 * index);
-            });
-            buttonMore.innerHTML = 'Load more';
-        } else {
-            resultsContainer.innerHTML = '<p>No results found or invalid query. <u><a href="/support">Help me out! 😰</a></u></p>';
-            buttonMore.innerHTML = 'Load more';
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        document.getElementById('results').innerHTML = '<p>An error occurred fetching the results.</p>';
-        buttonMore.innerHTML = 'Load more';
     });
 }
