3fdee548f70ea2199ba81b96e9190783eed55db2
Hidde Schultze <hidde@hidde.dev>
2024-06-24 10:17:44 +0200

    path fixes

--- src/js/placeholderTexts.js
+++ src/js/placeholderTexts.js
@@ -3,7 +3,7 @@ let inputElement = document.getElementById('query');
 let intervalId;
 
 function loadPlaceholders() {
-    fetch('/assets/placeholderTexts.txt')
+    fetch('/js/placeholderTexts.txt')
         .then(response => response.text())
         .then(text => {
             placeholders = text.split('\n').filter(Boolean);  // Ensure no empty strings
