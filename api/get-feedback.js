ca44696e0e4aa5c9a8d0119e24239af9374d0d09
tychografie <hoi@tycholitjens.nl>
2024-08-06 09:55:08 +0200

    Authenthication back up

--- api/get-feedback.js
+++ api/get-feedback.js
@@ -1,102 +1,123 @@
-const { MongoClient, ObjectId } = require('mongodb');
-const { validateToken } = require('./lib/auth');
-
-module.exports = async (req, res) => {
-    if (!req.headers["authorization"]) {
-        res.status(401).json({});
-        return 
-    } else {
-        const token = req.headers["authorization"].split("Bearer ")[1];
-        console.log(token);
-        if (!token) return res.status(401).json({error:"Invalid Token"});
-        if (!validateToken(token)) {
-            // expired or invalid token
-            return res.status(401).json({error:"Invalid Token"});
-        }
-    }
-
-    const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
+document.addEventListener("DOMContentLoaded", async () => {
+    const userToggle = document.getElementById("userToggle");
+    userToggle.addEventListener("change", () =>
+      loadFeedbackData(userToggle.checked)
+    );
+    await loadFeedbackData(userToggle.checked);
+  });
+  
+  async function authorize() {
+    // get user input
+    const password = prompt("Password:");
+    // get token
     try {
-        await client.connect();
-        const database = client.db('tychomapsmongodb');
-        const collection = database.collection('searches');
-
-        if (req.method === "POST") {
-            const { rating, id, feedbackHandled } = req.body;
-
-            if (rating !== undefined) {
-                if (rating !== 1 && rating !== -1) {
-                    return res.status(400).json({ error: "Invalid rating" });
-                }
-
-                const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-                const mostRecentSearch = await collection.findOne(
-                    { ip: ip },
-                    { sort: { timestamp: -1 } }
-                );
-
-                if (mostRecentSearch) {
-                    await collection.updateOne(
-                        { _id: mostRecentSearch._id },
-                        { $set: { userRating: rating } }
-                    );
-                    return res.status(200).json({ message: "Feedback recorded successfully" });
-                } else {
-                    return res.status(404).json({ error: "No recent search found" });
-                }
-            } else if (id !== undefined && feedbackHandled !== undefined) {
-                if (typeof feedbackHandled !== 'boolean') {
-                    return res.status(400).json({ error: "Invalid feedbackHandled value" });
-                }
-
-                await collection.updateOne(
-                    { _id: new ObjectId(id) },
-                    { $set: { feedbackHandled: feedbackHandled ? 1 : 0 } }
-                );
-                return res.status(200).json({ message: "Feedback handled status updated successfully" });
-            } else {
-                return res.status(400).json({ error: "Invalid request body" });
-            }
-        } else if (req.method === "GET") {
-            const { rating, resultCountFilter } = req.query;
-
-            let query = {};
-            if (rating) {
-                const ratings = rating.split(',').map(Number);
-                query.userRating = { $in: ratings };
-            }
-
-            if (resultCountFilter) {
-                let resultCountQuery;
-                switch (resultCountFilter) {
-                    case '0':
-                        resultCountQuery = 0;
-                        break;
-                    case '0-1':
-                        resultCountQuery = { $lte: 1 };
-                        break;
-                    case '2-5':
-                        resultCountQuery = { $gte: 2, $lte: 5 };
-                        break;
-                    case '5+':
-                        resultCountQuery = { $gte: 5 };
-                        break;
-                    default:
-                        break;
-                }
-                query.resultCount = resultCountQuery;
-            }
-
-            const feedback = await collection.find(query).sort({ timestamp: -1 }).toArray();
-            return res.status(200).json(feedback);
-        } else {
-            return res.status(405).json({ error: "Method not allowed" });
-        }
+      const authResponse = await fetch(`/api/authorize`, {
+        method: "POST",
+        body: window.btoa(password),
+      });
+      if (!authResponse.ok) {
+        console.error(error);
+        location.href = "/";
+        return;
+      }
+      const token = (await authResponse.json()).token;
+      localStorage.setItem("token", token);
     } catch (error) {
-        console.error('Error handling feedback:', error);
-        return res.status(500).json({ error: "Internal server error" });
-    } finally {
-        await client.close();
+      console.error(error);
+      location.href = "/";
+      return;
     }
-};
+  }
+  
+  async function loadFeedbackData(showHappyUsers) {
+    if (
+      !localStorage.getItem("token") ||
+      localStorage.getItem("token") === "undefined"
+    ) {
+      await authorize();
+    }
+    try {
+      const response = await fetch(
+        `/api/feedback?rating=${showHappyUsers ? 1 : -1}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+  
+      const data = await response.json();
+      document.querySelector("body").style.display = "block";
+      renderFeedbackList(data);
+    } catch (error) {
+      localStorage.removeItem("token");
+      await authorize();
+    }
+  }
+  
+  async function renderFeedbackList(data) {
+    const feedbackList = document.getElementById("feedback-list");
+    feedbackList.innerHTML = `
+          <div class="flex items-center p-4 border-b border-gray-200 font-bold">
+              <span class="w-1/12">Done?</span>
+              <span class="w-2/12 min-w-0">When?</span>
+              <span class="w-3/12 min-w-0">Query</span>
+              <span class="w-full min-w-0">Maps Request</span>
+              <span class="w-1/12 hidden md:flex min-w-0">Result Count</span>
+          </div>
+      `;
+    data.forEach((item) => {
+      const row = document.createElement("div");
+      row.className = "flex items-center p-4 border-b border-gray-200";
+  
+      const checkboxContainer = document.createElement("span");
+      checkboxContainer.className = "w-1/12 flex justify-center";
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.checked = item.feedbackHandled === 1;
+      checkbox.addEventListener("change", () =>
+        handleCheckboxChange(item._id, checkbox.checked)
+      );
+      checkboxContainer.appendChild(checkbox);
+  
+      const queryText = document.createElement("span");
+      queryText.className = "w-3/12 min-w-0 truncate";
+      queryText.textContent = item.query;
+  
+      const mapsRequestText = document.createElement("span");
+      mapsRequestText.className = "w-full min-w-0 truncate";
+      mapsRequestText.textContent = item.mapsRequest;
+  
+      const resultCountText = document.createElement("span");
+      resultCountText.className = "w-1/12 min-w-0 truncate hidden md:flex";
+      resultCountText.textContent = item.resultCount;
+  
+      const timestampText = document.createElement("span");
+      timestampText.className = "w-2/12 min-w-0 truncate";
+      const date = dateFns.parseISO(item.timestamp);
+      timestampText.textContent = dateFns.formatDistanceToNow(date, {
+        addSuffix: true,
+      });
+  
+      row.appendChild(checkboxContainer);
+      row.appendChild(timestampText);
+      row.appendChild(queryText);
+      row.appendChild(mapsRequestText);
+      row.appendChild(resultCountText);
+  
+      feedbackList.appendChild(row);
+    });
+  }
+  
+  async function handleCheckboxChange(id, isChecked) {
+    await fetch("/api/feedback", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, feedbackHandled: isChecked }),
+    });
+  }
+  
