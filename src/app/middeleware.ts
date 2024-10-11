d6ef0cc08f14b0c8061279d10cc6e85aa3044236
Hidde Schultze <hidde@hidde.dev>
2024-10-11 15:45:31 +0200

    Add middleware for rate limiting

--- src/app/middeleware.ts
+++ src/app/middeleware.ts
@@ -1,25 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { Ratelimit } from '@upstash/ratelimit';
-import { kv } from '@vercel/kv';
+import { NextRequest, NextResponse } from 'next/server'
+import { Ratelimit } from '@upstash/ratelimit'
+import { kv } from '@vercel/kv'
 
 const ratelimit = new Ratelimit({
   redis: kv,
   // 5 requests from the same IP in 10 seconds
   limiter: Ratelimit.slidingWindow(5, '10 s'),
-});
+})
 
 // Define which routes you want to rate limit
 export const config = {
-  matcher: '/',
-};
+  matcher: ['/', 'api/search'],
+}
 
-export default async function middleware(request: NextRequest) {
+export default async function middleware (request: NextRequest) {
   // You could alternatively limit based on user ID or similar
-  const ip = request.ip ?? '127.0.0.1';
+  const ip = request.ip ?? '127.0.0.1'
   const { success, pending, limit, reset, remaining } = await ratelimit.limit(
-    ip
-  );
+    ip,
+  )
   return success
     ? NextResponse.next()
-    : NextResponse.redirect(new URL('/blocked', request.url));
+    : NextResponse.redirect(new URL('/blocked', request.url))
 }
