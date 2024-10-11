f30f6a9191ec5460259af3cdeb8e928127b4f1b4
Hidde Schultze <hidde@hidde.dev>
2024-10-11 14:13:27 +0200

    Font and style fixes

--- src/app/action.ts
+++ src/app/action.ts
@@ -4,12 +4,12 @@ import { SearchStateResponse } from '@/app/types'
 
 export async function search (
   prevState: any, formData: FormData): Promise<SearchStateResponse> {
-  return await new Promise(resolve => {
-    setTimeout(() => {
-      console.log('query:', formData.get('query'))
-      resolve({
+  // return await new Promise(resolve => {
+  //   setTimeout(() => {
+  //     console.log('query:', formData.get('query'))
+      return{
         message: 'search : ' + formData.get('query'),
-      })
-    }, 2000)
-  })
+      }
+    // }, 2000)
+  // })
 }
